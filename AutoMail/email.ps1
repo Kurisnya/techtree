86cee76262295559ff10464c81d4f45d66915868
Kurisnya <christiansoaresdiniz@gmail.com>
2026-01-21 15:20:33 -0300

    Delete AutoMail directory

--- AutoMail/email.ps1
+++
@@ -1,8 +0,0 @@
-﻿$outlook = New-Object -ComObject Outlook.Application
-$mail = $outlook.CreateItem(0)
-$dados = Import-Csv (Join-Path $PSScriptRoot "MailList.csv")
-
-foreach ($linha in $dados) {
-		
- 	.\email2 $linha.Email
-}
