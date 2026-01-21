bfcd663a3b014ca2b274cbb6e10921ee902e35a0
Kurisnya <christiansoaresdiniz@gmail.com>
2026-01-21 15:19:00 -0300

    Delete email.ps1

--- email.ps1
+++
@@ -1,8 +0,0 @@
-$outlook = New-Object -ComObject Outlook.Application
-$mail = $outlook.CreateItem(0)
-$dados = Import-Csv "C:\Users\christian.soares\bin\AutoMail\MailList.csv"
-
-foreach ($linha in $dados) {
-		
- 	email2 $linha.Email
-}
