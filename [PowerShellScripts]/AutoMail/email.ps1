46ff86decfe95291bd7969111fb656ba1c6f3470
Kurisnya <christiansoaresdiniz@gmail.com>
2026-01-21 15:21:21 -0300

    Add files via upload

---
+++ [PowerShellScripts]/AutoMail/email.ps1
@@ -0,0 +1,8 @@
+﻿$outlook = New-Object -ComObject Outlook.Application
+$mail = $outlook.CreateItem(0)
+$dados = Import-Csv (Join-Path $PSScriptRoot "MailList.csv")
+
+foreach ($linha in $dados) {
+		
+ 	.\email2 $linha.Email
+}
